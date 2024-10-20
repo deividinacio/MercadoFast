cbbbe4c7b54b37f432914d56f6488d1220e567dc
deividinacio <deivid_@outlook.com>
2024-10-20 12:43:19 -0300

    modifica funcao myfuncoes.js para pegar title

--- js/myfuncoes.js
+++ js/myfuncoes.js
@@ -2,6 +2,7 @@
 //pega Ano Atual e coloca no paragrafo através do id = copyright_p
 var dataAtual  = new Date();
 var anoAtual = dataAtual.getFullYear();
-copyright_p.innerHTML = "Copyright © " +  anoAtual + " MercadoFast";
+var pegaTitle = document.title;
+copyright_p.innerHTML = "Copyright © " + anoAtual + " " + pegaTitle;
 
 
